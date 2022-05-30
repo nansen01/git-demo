f0c539910be6de040f104a7587a2f38ad601dac6
nansen <2273823176@qq.com>
2022-05-30 19:04:05 +0800

    GitDemo

--- src/main/java/com/nansen/git/GitDemo.java
+++ src/main/java/com/nansen/git/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello,Git");
         System.out.println("Hello,GitHub");
+        System.out.println("Hello,GitHub2");
     }
 }
